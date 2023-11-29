bc947cc6816e73a0d59cf49ac3bfbc9a016aa965
Aslon <aslon.dinov@gmail.com>
2023-11-29 10:45:23 +0100

    Projects

---
+++ src/main/java/array.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+
+
+        int[] array1 = new int[10];
+
+        array1[0] = 0;
+        array1[1] = 1;
+        array1[2] = 2;
+        array1[3] = 3;
+        array1[4] = 4;
+        array1[5] = 5;
+        array1[6] = 6;
+        array1[7] = 7;
+        array1[8] = 8;
+        array1[9] = 9;
+
+        // Step 2
+        int sum = 0;
+
+        for (int i = 0; i < array1.length; i++) {
+            sum += array1[i];
+        }
+        System.out.println(sum);
+
+        // Step 3
+        int max = array1[0];
+
+        for (int j : array1) {
+
+            if (j > max) {
+
+                max = j;
+            }
+        }
+        System.out.println(max);
+
+
+        // Step 4
+        int[] array2 = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+
+        // Step 5
+        int[] array3 = new int[10];
+
+        for (int i = 0; i < array3.length; i++) {
+            array3[i] = array1[i] + array2[i];
+            System.out.println(array3[i]);
+        }
+
+
+        // bonus
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a text:");
+        String text = scanner.nextLine();
+
+        for(int i = 0; i < text.length(); i++) {
+            System.out.println(text.charAt(i));
+        }
+    }
+
+}
+
+
---
+++ src/main/java/myJava.java
@@ -0,0 +1,79 @@
+import java.lang.reflect.Array;
+
+public class myJava {
+    public static void main(String[] args) {
+
+        System.out.println(add(5, 6));
+        System.out.println(add(5.12,  6.15));
+        System.out.println(subtract(5.12,  6.15));
+        System.out.println(makePositive(-5));
+
+        System.out.println(checkNumbers(15));
+        System.out.println(isPalindrome("ollo"));
+        System.out.println(isReverse("ollo"));
+
+        // Bonus
+        int a = 10, b = 8;
+        if(a>b){
+            System.out.println(subtract(a,  b));
+        }
+    }
+
+    public static int add(int num1,int num2) {
+
+         int sum = num1+num2;
+         return sum;
+    }
+    public static double add(double num1, double num2) {
+
+         double sum = num1 + num2;
+         return sum;
+    }
+    public static double subtract(double num1, double num2) {
+
+         double result = num1 - num2;
+         return result;
+    }
+    public static double makePositive(int num1) {
+
+        if(num1>0){
+            return num1;
+        }else {
+            return -num1;
+        }
+
+    }
+
+    // Bonus
+    public static int checkNumbers(int a) {
+
+       if (a%2==0){
+           return a;
+       }else {
+          return 2*a;
+       }
+    }
+
+    public static boolean isPalindrome(String args) {
+
+      boolean palindrome = true;
+
+        int n = args.length();
+
+        for (int i = 0; i < n/2 ; i++) {
+
+                if (args.charAt(i) != args.charAt(n-i-1)){
+                    palindrome = false;
+                }
+
+        }
+         return palindrome;
+    }
+
+    public static boolean isReverse(String args) {
+
+        return new StringBuilder(args).reverse().toString().equals(args);
+
+    }
+}
+
